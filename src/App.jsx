ca16e16f0c5166947aab52e61a76944997986f5c
Von Lycaon <thes01475@gmail.com>
2024-09-09 10:47:43 +0700

    updt

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import video from './image/Background2.mp4'
 import Navbar from './Component/Navbar/Navbar'
 import car1 from './image/AE86.jpg'
 import car2 from './image/Ferrari1987.jpg'
-import wolf1 from './image/Wolf1.jpg'
 
 function App() {
   return (
@@ -84,25 +83,30 @@ function App() {
 
         <div className="box1"></div>
 
-        <div className="Contact" id='Contact'>
+        <div className="Page" id='Page'>
           <div className="container">
-            <div className="Owner">
+            <h1>Latest News & Article:</h1>
+            <div className="Article">
+            <div className="Blog1">
               <div className="image"></div>
               <div className="text">
-                <h1>Contact:</h1>
-                <ul>
-                  <li>Email: Example@gmail.com</li>
-                  <li>Phone: +1-111-111</li>
-                </ul>
+                <h1>Lorem ipsum dolor sit amet.</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, ratione impedit corrupti rerum possimus quod quasi voluptatum velit officiis accusamus nihil enim pariatur eaque nam ipsam eveniet reprehenderit expedita veritatis delectus magnam laudantium ducimus alias maxime? Magni cupiditate sed quod.</p>
               </div>
+            </div>
+            <div className="Blog2">
+              <div className="image"></div>
+              <div className="text">
+                <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo sunt consequuntur assumenda? Perferendis rerum ipsum magni nostrum? Totam voluptas dolore animi quam! Veritatis distinctio quae perferendis ullam quam necessitatibus alias eos exercitationem iure possimus fugiat autem repellat, unde itaque! Repellat, velit maxime excepturi obcaecati aliquam tempora. Perferendis aut illo quaerat.</p>
+              </div>
+            </div>
             </div>
           </div>
         </div>
 
-        <div className="box2"></div>
-
-        <div className="Page">
-
+        <div className="Contact" id='Contact'>
+          
         </div>
       </main>
     </>
--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -13,8 +13,8 @@ const Navbar = () =>{
                     <li><a href="#Home">Home</a></li>
                     <li><a href="#About Us">About Us</a></li>
                     <li><a href="#Services">Services</a></li>
-                    <li><a href="#Contact">Contact</a></li>
                     <li><a href="#Page">Page</a></li>
+                    <li><a href="#Contact">Contact</a></li>
                 </ul>
            </div>
         </nav>
